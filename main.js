49e60fda7790578734ca481a318c61228c19abc9
Ignacio Lopezosa <ilopezosaserrano@gmail.com>
2022-03-14 20:28:48 +0100

    v-bind learned for linking html tag attributes to json attributes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://youtu.be/bzlFvd0b65c?t=766'
         }
     }
 })
